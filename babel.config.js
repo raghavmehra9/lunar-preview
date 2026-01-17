bdda76a7680535625c34a5624b17b07904db5c60
raghavmehra9 <raghavmehra8991@gmail.com>
2026-01-16 18:50:51 -0800

    Enable @ alias for web build

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': '.',
+          },
+        },
+      ],
     ],
-    plugins: ["react-native-worklets/plugin"],
   };
 };
+
+
